bc1f3273f95a781012e6f0032d53d99751dcdbef
AmanJangir07 <117352220+AmanJangir07@users.noreply.github.com>
2023-09-14 11:39:34 +0530

    Update 'Shopiana Store' to 'Amy Collections'

--- lib/utill/store_constant.dart
+++ lib/utill/store_constant.dart
@@ -2,6 +2,6 @@ class StoreConstant {
   static const STORE_CODE =
       "default"; /* "sleepin22102001" */ //"default"; //'feathermat22082801';
   // 'shahi22080501'; // 'keshav22062401'; //'default'; //'fast22050701';
-  static const STORE_NAME = 'Shopiana Store';
+  static const STORE_NAME = 'Amy Collections';
   static const STORE_ANDROID_PACKAGE = 'com.shopiana.shopiana';
 }
